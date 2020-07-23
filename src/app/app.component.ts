5ff0ad992ecda303e492603e67166eb790a4111d
Andrei Voicu <dreivoicu@gmail.com>
2020-07-23 07:57:07 +0300

    dummy validator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, AbstractControl, ValidationErrors } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  control = new FormControl(null, dummyValidator);
+}
+
+function dummyValidator(control: AbstractControl): ValidationErrors | null {
+  console.log('checking...');
+
+  return null;
 }
