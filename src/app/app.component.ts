d169281a96f8a330967fc36e7416256fdfad1220
Andrei Voicu <dreivoicu@gmail.com>
2020-07-23 08:20:31 +0300

    lazy validator with debounce

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import {
   FormControl,
   AbstractControl,
   ValidationErrors,
   Validators
 } from '@angular/forms';
+import { Subject } from 'rxjs';
+import { takeUntil, debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  control = new FormControl(null, {
+export class AppComponent implements OnInit, OnDestroy {
+  private unsubscribe$: Subject<void> = new Subject();
+
+  blurControl = new FormControl(null, {
     validators: Validators.minLength(4),
     updateOn: 'blur'
   });
+
+  lazyControl = new FormControl(null);
+
+  ngOnInit(): void {
+    this.lazyControl.valueChanges
+      .pipe(debounceTime(300), takeUntil(this.unsubscribe$))
+      .subscribe(() => {
+        this.lazyControl.setErrors(Validators.minLength(3)(this.lazyControl));
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+  }
 }
 
 function dummyValidator(control: AbstractControl): ValidationErrors | null {
